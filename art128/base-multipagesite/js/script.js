dbeb72227a68d359568bcc9f2bfd0ad189483adb
trezamora <tzamora@hawaii.edu>
2022-02-10 01:42:05 -1000

    single page site (get open close menu to work)

---
+++ art128/base-multipagesite/js/script.js
@@ -0,0 +1,15 @@
+document.addEventListener('DOMContentLoaded', function() {
+
+/* TOGGLE NAV MOBILE MENU FOR SMALL SCREENS */
+ const menubuton = document.querySelector('.menu-button');
+ const menunav = document.querySelector('.toggle-nav');
+ menubuton.addEventListener('click', function() {
+    if (menunav.getAttribute('data-navstate') === 'open') {
+        /* if true do this */
+        menunav.setAttribute('data-navstate', 'closed');
+    } else {
+        /* else (if false) do this */
+        menunav.setAttribute('data-navstate', 'open');
+    };
+ });
+});
