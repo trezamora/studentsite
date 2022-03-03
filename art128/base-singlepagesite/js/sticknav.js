a18e332054b87c03e476f1cb65820853ded3e97f
trezamora <tzamora@hawaii.edu>
2022-03-03 10:14:34 -1000

    edit single site js

--- art128/base-singlepagesite/js/sticknav.js
+++ art128/base-singlepagesite/js/sticknav.js
@@ -1,12 +1,18 @@
 document.addEventListener("DOMContentLoaded", function () {
 
     //STICKY NAV LINKS TOGGLE MENU NAV
-    var stickynavlinks = document.querySelectorAll(".sticky nav a");
-    var stickyheader = document.querySelector(".sticky");
-    var j;
+    const stickynavlinks = document.querySelectorAll(".sticky nav a");
+    const stickyheader = document.querySelector(".sticky");
+    let j;
     for (j = 0; j < stickynavlinks.length; j++) {
         stickynavlinks[j].onclick = function () {
             stickyheader.setAttribute('data-navstate', 'closed');
         };
+    };
+
+    /* SITD ID TOGGLE NAV CLOSE MENU NAV */
+    const stickyidlink = document.querySelector(".sticky .site-id a");
+    stickyidlink.onclick = function () {
+        stickyheader.setAttribute('data-navstate', 'closed');
     };
 });
